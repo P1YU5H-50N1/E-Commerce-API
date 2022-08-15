11164816c8a9fd881eb1ed58f5b3c45f821e9677
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 16:07:44 +0530

    Completed Controllers

--- controllers/buyerContollers.js
+++ controllers/buyerContollers.js
@@ -1,19 +1,92 @@
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 
+const User = require("../model/userModel");
+const Item = require("../model/itemModel");
+const Order = require("../model/orderModel");
+const OrderItem = require("../model/orderItemModel");
+
+//@desc list all sellers
+//@route POST /api/auth/login
+//@access private
 const getSellerList = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Seller list" });
+	const sellers = await User.find({ user_type: "SELLER" });
+	const user_sellers = sellers.map((val) => {
+		return {
+			_id: val._id,
+			name: val.name,
+			email: val.email,
+		};
+	});
+	res.status(200).json({ sellers: user_sellers });
 });
 
+//@desc get catalog items for a seller
+//@route POST /api/auth/login
+//@access private
 const getCatalog = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Get Catalog", id:req.params.seller_id });
+	const seller_id = req.params.seller_id;
+	const seller = await User.findOne({ _id: seller_id });
+	const items = await Item.find({ seller_id: seller });
+	res.status(200).json({
+		message: "Get Catalog",
+		id: req.params.seller_id,
+		item_list: items,
+	});
 });
 
+//@desc create order
+//@route POST /api/auth/login
+//@access private
 const createOrder = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Order Created",id:req.params.seller_id  });
+	if (!req.user) {
+		res.status(401);
+		throw new Error("User not found");
+	}
+
+	const order_items = req.body.items;
+	const items = order_items.map(async (item) => {
+		let current_item = await Item.findOne({
+			name: item.name,
+			seller_id: mongoose.Types.ObjectId(item.seller_id),
+		});
+		current_item["quantity"] = item["quantity"];
+		return current_item;
+	});
+	const all_items = items.reduce((all_good, item) => {
+		if (item) {
+			all_good = all_good && all_good;
+		} else {
+			all_good = false;
+		}
+	}, true);
+	if (!all_items) {
+		res.status(400);
+		throw new Error("Invalid Items");
+	}
+	const new_order = await Order.create({
+		buyer_id: req.user.id,
+		seller_id: mongoose.Types.ObjectId(req.body.seller_id),
+	});
+
+	const new_order_items = order_items.map(async (order_item) => {
+		let add_item = await OrderItem.create({
+			seller_id: order_item.seller_id,
+			order_id: new_order._id,
+			quantity: order_item.quantity,
+		});
+		return add_item;
+	});
+
+	res.status(200).json({
+		message: "Order Created Successfully",
+		id: req.params.seller_id,
+		items: new_order_items,
+	});
 });
 
 module.exports = {
 	getSellerList,
 	getCatalog,
-	createOrder
-}
+	createOrder,
+};
--- controllers/sellerControllers.js
+++ controllers/sellerControllers.js
@@ -1,13 +1,27 @@
-const asyncHandler = require('express-async-handler')
-
+const asyncHandler = require("express-async-handler");
+const Order = require("../model/orderModel");
+const OrderItem = require("../model/orderItemModel");
+const Item = require("../model/itemModel");
+const { default: mongoose } = require("mongoose");
 
 const sellerOrders = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "seller Orders" });
-})
+	const orders = await Order.find({ _id: req.user.id });
+	const orders_success = orders.map(async (order, idx) => {
+		let items = await OrderItem.find({ order_id: order._id });
+		order.items = items;
+		return order;
+	});
+	res.status(200).json({ message: "seller Orders", orders: orders });
+});
 
 const createCatalog = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Created catalog" });
-})
+	const items = req.body.items.map((item) => {
+		item.seller_id = mongoose.Types.ObjectId(req.user.id);
+		return item;
+	});
+	const items_success = await Item.insertMany(items);
+	res.status(200).json({ message: "Created catalog", items: items_success });
+});
 
 module.exports = {
 	sellerOrders,
