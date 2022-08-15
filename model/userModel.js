27b4b083c3d3d1c660dde1aaccb10d2b6f40fd73
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 12:43:13 +0530

    Implemented user model

---
+++ model/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please add a name"],
+		},
+		email: {
+			type: String,
+			required: [true, "Please add a name"],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Please add a name"],
+		},
+        user_type: {
+            type: String,
+            enum: ['BUYER','SELLER'],
+            default: 'BUYER'
+        }
+	},
+	{
+		timestamps: true,
+	}
+);
+
+
+module.exports = mongoose.model('User',userSchema)
