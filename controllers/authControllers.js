c2418d5499ed2b01639de40dee3ddc5b45e9fc1e
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 18:35:07 +0530

    Input validation

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -48,7 +48,11 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
 
-	// Check for user email
+	if (!email || !password) {
+		res.status(404);
+		throw new Error("Please Add all fields");
+	}
+
 	const user = await User.findOne({ email });
 
 	if (user && (await bcrypt.compare(password, user.password))) {
