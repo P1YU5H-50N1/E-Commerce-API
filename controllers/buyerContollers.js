b51b7e1f011510823551dea8d46eb3e618d0496d
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 18:37:50 +0530

    Comments & input validation

--- controllers/buyerContollers.js
+++ controllers/buyerContollers.js
@@ -7,9 +7,13 @@ const Order = require("../model/orderModel");
 const OrderItem = require("../model/orderItemModel");
 
 //@desc list all sellers
-//@route POST /api/auth/login
+//@route POST /api/buyer/list-of-sellers
 //@access private
 const getSellerList = asyncHandler(async (req, res) => {
+	if (req.user.user_type !== "BUYER") {
+		res.status(401);
+		throw new Error("Not authorized");
+	}
 	const sellers = await User.find({ user_type: "SELLER" });
 	const user_sellers = sellers.map((val) => {
 		return {
@@ -22,66 +26,87 @@ const getSellerList = asyncHandler(async (req, res) => {
 });
 
 //@desc get catalog items for a seller
-//@route POST /api/auth/login
+//@route GET /api/buyer/seller-catalog/:seller_id
 //@access private
 const getCatalog = asyncHandler(async (req, res) => {
+	if (req.user.user_type !== "BUYER") {
+		res.status(401);
+		throw new Error("Not authorized");
+	}
+
 	const seller_id = req.params.seller_id;
+	if (!mongoose.Types.ObjectId.isValid(seller_id)) {
+		res.status(400);
+		throw new Error("Invalid seller id");
+	}
+
 	const seller = await User.findOne({ _id: seller_id });
+
+	if (!seller) {
+		res.status(400);
+		throw new Error("Invalid seller id");
+	}
+
 	const items = await Item.find({ seller_id: seller });
 	res.status(200).json({
 		message: "Get Catalog",
-		id: req.params.seller_id,
+		seller_id: req.params.seller_id,
 		item_list: items,
 	});
 });
 
 //@desc create order
-//@route POST /api/auth/login
+//@route POST /api/buyer/create-order/:seller_id
 //@access private
 const createOrder = asyncHandler(async (req, res) => {
 	if (!req.user) {
 		res.status(401);
 		throw new Error("User not found");
 	}
+	if (req.user.user_type !== "BUYER") {
+		res.status(401);
+		throw new Error("Not authorized");
+	}
+	const seller_id = req.params.seller_id;
+	if (!mongoose.Types.ObjectId.isValid(seller_id)) {
+		res.status(400);
+		throw new Error("Invalid seller id");
+	}
 
 	const order_items = req.body.items;
-	const items = order_items.map(async (item) => {
-		let current_item = await Item.findOne({
-			name: item.name,
-			seller_id: mongoose.Types.ObjectId(item.seller_id),
-		});
-		current_item["quantity"] = item["quantity"];
-		return current_item;
-	});
-	const all_items = items.reduce((all_good, item) => {
-		if (item) {
-			all_good = all_good && all_good;
-		} else {
-			all_good = false;
-		}
-	}, true);
-	if (!all_items) {
-		res.status(400);
-		throw new Error("Invalid Items");
-	}
+	const items = await Promise.all(
+		order_items.map(async (item) => {
+			let current_item = await Item.findOne({
+				name: item.name,
+				seller_id: mongoose.Types.ObjectId(seller_id),
+			});
+			if (!current_item) {
+				res.status(400);
+				throw new Error("Invalid Items Or Seller Id");
+			}
+			current_item["quantity"] = item["quantity"] || 1;
+			return current_item;
+		})
+	);
+
 	const new_order = await Order.create({
 		buyer_id: req.user.id,
-		seller_id: mongoose.Types.ObjectId(req.body.seller_id),
+		seller_id: mongoose.Types.ObjectId(seller_id),
 	});
 
-	const new_order_items = order_items.map(async (order_item) => {
-		let add_item = await OrderItem.create({
-			seller_id: order_item.seller_id,
-			order_id: new_order._id,
-			quantity: order_item.quantity,
-		});
-		return add_item;
+	const new_order_items = items.map((item) => {
+		return {
+			seller_id: item.seller_id,
+			order_id: new_order,
+			quantity: item.quantity,
+		};
 	});
+	const inserted_items = await OrderItem.insertMany(new_order_items);
 
 	res.status(200).json({
 		message: "Order Created Successfully",
-		id: req.params.seller_id,
-		items: new_order_items,
+		id: seller_id,
+		items: inserted_items,
 	});
 });
 
