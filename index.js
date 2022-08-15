acf9bb8de5137dfcda9784f677dda5cfb0d3e4e9
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 12:41:18 +0530

    DONE: Middleware for body parsing

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require("express");
 const dotenv = require("dotenv").config()
 const authRoutes = require('./routes/auth/authRoutes')
 const buyerRoutes = require('./routes/buyer/buyerRoutes')
-const sellertRoutes = require('./routes/seller/sellerRoutes')
+const sellerRoutes = require('./routes/seller/sellerRoutes')
+const connectDB = require('./config/db');
+const { connect } = require("./routes/seller/sellerRoutes");
 
+connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/auth',authRoutes)
-app.use('/api/buyer',authRoutes)
-app.use('/api/seller',authRoutes)
+app.use('/api/buyer',buyerRoutes)
+app.use('/api/seller',sellerRoutes)
 
 
-const port = process.env.PORT||5000
+const port = process.env.PORT || 5000
 
 
 app.listen(port,()=> console.log(`Server started at ${port}`))
